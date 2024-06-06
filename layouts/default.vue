156a2913bf7165b6179608b8d08e724735ceaa24
ulugbek-mominjonov <ulugbeknabijonovich@gmail.com>
2024-06-06 13:38:32 +0500

    add some changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+  const containerConfig = {
+  base: 'mx-auto',
+  padding: 'px-4 sm:px-6 lg:px-8',
+  constrained: 'max-w-7xl',
+}
+
+</script>
 <template>
-  <div>
-    <slot></slot>
-  </div>
+  <UContainer :ui="containerConfig">
+    <HeaderAppHeader />
+    <main>
+      <Placeholder class="h-32" />
+      <slot></slot>
+    </main>
+    <FooterAppFooter />
+  </UContainer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,6 @@
 <template>
   <div>
     <h1>Home page</h1>
-    <NuxtLink to="/about">About page</NuxtLink>
+    <NuxtLink class="text-blue-500" to="/about">About page</NuxtLink>
   </div>
 </template>
